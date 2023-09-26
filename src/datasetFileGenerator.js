e54acf4d82dc5fb6005f7b9d314b71f137a6547c
Miguel Naranjo <miguel.naranjo@perficient.com>
2023-09-26 10:52:49 -0500

    Adding template formater for word and excel

--- src/datasetFileGenerator.js
+++ src/datasetFileGenerator.js
@@ -6,6 +6,7 @@ import { generateDatasetFileContent } from "./api.js";
 import { formatFilename } from './utils/formatFilename.js';
 import PDFDocument from 'pdfkit';
 import { consoleWrite } from './utils/consoleWrite.js';
+import {templateWrite} from "./utils/templateWrite.js";
 
 export default async function (indexFilename, filesType) {
 
@@ -20,13 +21,25 @@ export default async function (indexFilename, filesType) {
     const fileContent = await generateDatasetFileContent(filename);
 
     const filePath = `${directory}/${formatFilename(filename, filesType)}`;
-    await writeFile(filePath, fileContent, filesType);
+    await writeFile(filePath, fileContent, filesType, filename);
     consoleWrite(`File "${filePath}" generated.`);
   }
 }
 
-async function writeFile(filePath, fileContent, fileType) {
+async function writeFile(filePath, fileContent, fileType, filename) {
   switch(fileType) {
+    case 'docx':
+      const fileContentInTemplateDocx = await templateWrite(
+          './templates/format.docx', {title: filename, content: fileContent}
+      );
+      await fs.writeFile(filePath, fileContentInTemplateDocx);
+      break;
+    case 'xlsx':
+      const fileContentInTemplateXlsx = await templateWrite(
+          './templates/format.xlsx', {title: filename, content: fileContent.split('\n')}
+      );
+      await fs.writeFile(filePath, fileContentInTemplateXlsx);
+      break;
     case 'pdf':
       const doc = new PDFDocument();
       doc.pipe(createWriteStream(filePath));
